d4d8254a244bbac2d0123b261dc57d2d900e683a
jain-kushal <kushaljain@Kushals-MacBook-Pro.local>
2019-08-22 18:45:29 -0700

    basic structure of the form ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Form from './components/Form';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Contact Us</h1>
+			<Form />
 		</div>
 	);
 }
---
+++ src/components/form.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import '../assets/stylesheets/Form.css';
+
+export class Form extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			phone: Number,
+			email: '',
+			message: ''
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(evt) {
+		this.setState({ [evt.target.name]: evt.target.value });
+	}
+
+	handleSubmit(evt) {
+		evt.preventDefault();
+	}
+
+	render() {
+		return (
+			<div className="Form">
+				<MuiThemeProvider>
+					<React.Fragment>
+						<TextField
+							required
+							fullWidth
+							hintText="Enter your Name"
+							floatingLabelText="Name"
+							onChange={this.handleChange}
+							name="name"
+							className="Form-TextField"
+						/>
+						<TextField
+							required
+							fullWidth
+							hintText="Enter your phone"
+							floatingLabelText="Phone number"
+							onChange={this.handleChange}
+							name="phone"
+							className="Form-TextField"
+						/>
+						<TextField
+							required
+							fullWidth
+							hintText="Enter your email"
+							floatingLabelText="Email"
+							onChange={this.handleChange}
+							type="email"
+							name="email"
+							className="Form-TextField"
+						/>
+						<TextField
+							required
+							multiLine
+							fullWidth
+							hintText="Enter your message for us"
+							floatingLabelText="Message"
+							onChange={this.handleChange}
+							rows="5"
+							name="message"
+							className="Form-TextField Message"
+						/>
+						<RaisedButton
+							label="Submit"
+							primary={true}
+							onClick={this.handleSubmit}
+							className="Form-submitButton"
+						/>
+					</React.Fragment>
+				</MuiThemeProvider>
+			</div>
+		);
+	}
+}
+
+export default Form;
